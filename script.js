42faccee1ee6071be69f1bf96f68c12418faaee8
Nimesh <99685297+Nimeshashaluka@users.noreply.github.com>
2024-02-13 16:24:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function coloseBtn() {
+  const sidebar = document.querySelector(".sidebar");
+  sidebar.style.display = "none";
+}
+function menuBtn() {
+  const sidebar = document.querySelector(".sidebar");
+  sidebar.style.display = "flex";
+}
+
+// var typed = new typed(".typing", {
+//   Strings: ["developar", "car", "bike", "van"],
+//   typeSpeed: 100,
+//   backSpeed: 60,
+//   loop: true,
+// });
